5f6086a5519774d4ce07ca8640b54750d3009364
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-21 15:52:50 +0100

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,8 +2,11 @@ data "aws_region" "current" {}
 
 data "aws_eks_cluster" "cluster" {
   name = "myapps"
+  depends_on = [module.eks_cluster]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = "myapps"
+  depends_on = [module.eks_cluster]
+
 }
