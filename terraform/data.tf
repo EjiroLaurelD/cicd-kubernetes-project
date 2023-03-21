3c2c63f53190e6bb438a0c5ac0cdb2c62aca12f9
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-21 15:48:08 +0100

    Update data.tf
    fixed bug

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,9 +1,9 @@
 data "aws_region" "current" {}
 
-data "eks_cluster" "cluster" {
-  name = module.eks_cluster.cluster_name
+data "aws_eks_cluster" "cluster" {
+  name = "myapps"
 }
 
-data "eks_cluster_auth" "cluster" {
-  name = module.eks_cluster.cluster_name
+data "aws_eks_cluster_auth" "cluster" {
+  name = "myapps"
 }
