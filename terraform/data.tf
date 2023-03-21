e107a32d7b81f2da72357f8ec1d5667eeba61ab5
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-21 16:30:02 +0100

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,11 +2,8 @@ data "aws_region" "current" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks_cluster.cluster_id
-  id = "myapps"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks_cluster.cluster_id
-  id = "myapps"
-
 }
