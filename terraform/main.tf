cadecab2b055b67c0070976e6e52e7a0dede327b
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-22 08:25:45 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    version                = "1.13.3"
+
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     
@@ -44,7 +44,9 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  version                = "1.13.3"
+  experiments {
+    manifest_resource = true
+  }
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
