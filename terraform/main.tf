3806bc678051dfadff193b9ba4ba24072bf7711b
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-22 08:03:20 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,3 +47,4 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   
+} 
