c1e83638f746348652723ffaa75c718765d596b2
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-22 04:56:00 +0100

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,8 +1,8 @@
 data "aws_region" "current" {}
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks_cluster.cluster_name 
-}
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks_cluster.cluster_name 
+#}
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks_cluster.cluster_name 
