037fe5c422bcc0be5a4a7b8fc27aa0519235d444
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-22 08:11:59 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,9 +31,10 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
+    version                = "1.13.3"
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    load_config_file       = false 
+    
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
@@ -43,6 +44,7 @@ provider "helm" {
 }
 
 provider "kubernetes" {
+  version                = "1.13.3"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
